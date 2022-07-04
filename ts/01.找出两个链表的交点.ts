256135ebdbf2312642f894299df7df4a5d9736a8
张帮辉 <zhangbh@cnlemon.net>
2022-07-04 09:16:26 +0800

    feat: 文件夹初始化

---
+++ ts/01.找出两个链表的交点.ts
@@ -0,0 +1,8 @@
+/**
+ * leetcode: 160. 相交链表
+ * desciption: 给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表不存在相交节点，返回 null 。
+ */
+function getIntersectionNode(
+  headA: ListNode | null,
+  headB: ListNode | null
+): ListNode | null {}
