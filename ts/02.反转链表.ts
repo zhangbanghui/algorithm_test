68b2dd6b793b01d2449b60ae10636decaf5dfe5c
张帮辉 <zhangbh@cnlemon.net>
2022-08-04 10:33:03 +0800

    反转链表优化

--- ts/02.反转链表.ts
+++ ts/02.反转链表.ts
@@ -1,12 +1,23 @@
+// 递归
+function reverseList1(head: ListNode | null): ListNode | null {
+  if (head === null || head.next === null) return head;
+  const next = head.next;
+  const newHead = reverseList(next);
+  next.next = head;
+  head.next = null;
+  return newHead;
+}
+
+// 迭代
 function reverseList(head: ListNode | null): ListNode | null {
+  if (head === null || head.next === null) return head;
   let prev = null;
-  let cur = head;
-  let next = head;
-  while (cur !== null) {
-    next = cur.next;
-    cur.next = prev;
-    prev = cur;
-    cur = next;
+  let curr = head;
+  while (curr !== null) {
+    const next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
   }
   return prev;
 }
