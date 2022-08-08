dd2927cb1a3aec444528ce02946022710be0ffc7
张帮辉 <zhangbh@cnlemon.net>
2022-08-08 15:24:52 +0800

    最长和谐子序列 ts

---
+++ ts/48.最长和谐子序列.ts
@@ -0,0 +1,19 @@
+function findLHS(nums: number[]): number {
+  const sortArr = nums.sort((a, b) => a - b);
+
+  let begin = 0;
+  let ans = 0;
+
+  for (let end = 0; end < sortArr.length; end++) {
+    const current = sortArr[end];
+    let head = sortArr[begin];
+    if (current - head === 1) {
+      ans = Math.max(ans, end - begin + 1);
+    }
+    while (current - head > 1) {
+      begin += 1;
+      head = sortArr[begin];
+    }
+  }
+  return ans;
+}
