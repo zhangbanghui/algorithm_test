7df3c32e333dd10e539657c40854506c92a90cf1
张帮辉 <zhangbh@cnlemon.net>
2022-08-08 15:25:11 +0800

    --amend

--- ts/48.最长和谐子序列.ts
+++ ts/48.最长和谐子序列.ts
@@ -4,6 +4,7 @@ function findLHS(nums: number[]): number {
   let begin = 0;
   let ans = 0;
 
+  // 滑动窗口算法
   for (let end = 0; end < sortArr.length; end++) {
     const current = sortArr[end];
     let head = sortArr[begin];
